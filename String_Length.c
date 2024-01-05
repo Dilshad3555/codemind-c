d975668a9763a5f82d8808c82dc863c11fd15e90
Dilshad3555 <147487861+Dilshad3555@users.noreply.github.com>
2024-01-05 06:58:17 +0530

    #include <stdio.h> #include <string.h>
    int main()
    {
        char Str[1000];
        int i;
    
        scanf("%s", &Str);
     
        for (i = 0; Str[i] != NULL; ++i);
     
        printf("%d", i);
     
        return 0;
    }

---
+++ String_Length.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char Str[1000];
+    int i;
+
+    scanf("%s", &Str);
+ 
+    for (i = 0; Str[i] != NULL; ++i);
+ 
+    printf("%d", i);
+ 
+    return 0;
+}
